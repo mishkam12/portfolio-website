9f366f1355fa45dceaf82b964bae6ab9ea576747
Mishka Moodley <mishkammoodley@outlook.com>
2024-09-22 01:13:06 -0400

    Added transitions and animations

--- app.js
+++ app.js
@@ -1,60 +1,68 @@
+//Retrieve logo element
 const logo = document.getElementById("logo");
 
+//Retrieve navigation links
 const aboutMeNav = document.getElementById("about-me-nav");
 const projectsNav = document.getElementById("projects-nav");
 const skillsNav = document.getElementById("skills-nav");
-const contactInfoNav = document.getElementById("contact-info-nav");
 
-const welcomeSection = document.getElementById("welcome-section");
-const frontPage = document.getElementById("front-page");
-const aboutMeSection = document.getElementById("about-me");
-const projectsSection = document.getElementById("projects");
-const skillsSection = document.getElementById("skills");
-const contactInfo = document.getElementById("contact-info");
+//Retrieve divs of the page
+const frontPageDiv = document.getElementById("front-page");
+const aboutMeDiv = document.getElementById("about-me");
+const projectsDiv = document.getElementById("projects");
+const skillsDiv = document.getElementById("skills");
+const contactInfoDiv = document.getElementById("contact-info");
 
+//Retrieve buttons
 const learnMoreBtn = document.getElementById("learn-more-btn");
 const projectsBtn = document.getElementById("projects-btn");
 const skillsBtn = document.getElementById("skills-btn");
 
+//Event listener when "About Me" navigation is clicked
 aboutMeNav.addEventListener("click", event => {
     event.preventDefault();
-    frontPage.style.display = "none";
-    projectsSection.style.display = "none";
-    skillsSection.style.display = "none";
-    aboutMeSection.style.display = "block";
+    frontPageDiv.style.display = "none";
+    projectsDiv.style.display = "none";
+    skillsDiv.style.display = "none";
+    aboutMeDiv.style.display = "block";
 });
 
+//Event listener when "Projects" navigation is clicked
 projectsNav.addEventListener("click", event => {
     event.preventDefault();
-    frontPage.style.display = "none";
-    skillsSection.style.display = "none";
-    aboutMeSection.style.display = "none";
-    projectsSection.style.display = "block";
+    frontPageDiv.style.display = "none";
+    skillsDiv.style.display = "none";
+    aboutMeDiv.style.display = "none";
+    projectsDiv.style.display = "block";
 });
 
+//Event listener when "Skills" navigation is clicked
 skillsNav.addEventListener("click", event => {
     event.preventDefault();
-    frontPage.style.display = "none";
-    aboutMeSection.style.display = "none";
-    projectsSection.style.display = "none";
-    skillsSection.style.display = "block";
+    frontPageDiv.style.display = "none";
+    aboutMeDiv.style.display = "none";
+    projectsDiv.style.display = "none";
+    skillsDiv.style.display = "block";
 });
 
+//Event listener when "Learn More" button is clicked
 learnMoreBtn.addEventListener("click", event => {
     event.preventDefault();
-    frontPage.style.display = "none";
-    aboutMeSection.style.display = "block";
+    frontPageDiv.style.display = "none";
+    aboutMeDiv.style.display = "block";
 });
 
+//Event listener when "Projects" button is clicked
 projectsBtn.addEventListener("click", event => {
     event.preventDefault();
-    frontPage.style.display = "none";
-    projectsSection.style.display = "block";
+    frontPageDiv.style.display = "none";
+    projectsDiv.style.display = "block";
 });
 
+//Event listener when "See Skills" button is clicked
 skillsBtn.addEventListener("click", event => {
     event.preventDefault();
-    frontPage.style.display = "none";
-    skillsSection.style.display = "block";
+    frontPageDiv.style.display = "none";
+    skillsDiv.style.display = "block";
 });
  
